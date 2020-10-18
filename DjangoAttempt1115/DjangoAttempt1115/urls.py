d58c057f5e2a648aaa6e115be52bbec6dd4d81c4
Hadyn Lewis <hadyn.lewis96@gmail.com>
2020-10-18 14:20:32 -0500

    Proof of concept at url 127.0.0.0:port/app/1/

--- DjangoAttempt1115/DjangoAttempt1115/urls.py
+++ DjangoAttempt1115/DjangoAttempt1115/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 """
 
 # Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('app/',include('app.urls')),
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
